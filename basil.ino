3a3a429bcbbf6fa749227996efd47713ba6848f2
ner0tic <david.durost@gmail.com>
2016-09-21 22:18:44 -0400

    added rtc

--- basil.ino
+++ basil.ino
@@ -5,6 +5,9 @@
 */
 
 #include <Relay.h>
+#include <TimeLib.h>
+#include <Wire.h>
+#include <DS1307RTC.h>
 
 #define MPPT 2      // digital pin
 #define LED 13      // led pin
@@ -16,6 +19,13 @@ void setup()
     Serial.begin(9600);
     Serial.println("RaggedPi Project Codename Basil Initialized.");
   
+    setSyncProvider(RTC.get);
+    if(timeStatus()!= timeSet) {
+        Serial.println("Unable to sync with the RTC");
+    } else {
+        Serial.println("RTC has set the system time");
+    }
+
     fanRelay.begin();
 
     pinMode(MPPT, INPUT);
